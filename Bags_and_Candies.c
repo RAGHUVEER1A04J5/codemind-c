5c2f4ad1f0c1d6e7985ed409bee5b952c65e91da
RAGHUVEER1A04J5 <119720122+RAGHUVEER1A04J5@users.noreply.github.com>
2022-12-20 21:31:09 +0530

    #include<stdio.h> int main() {     int n,k,m;     scanf("%d%d%d",&n,&k,&m);     int a=k*m,b=n%a;     if(b==0 && a<=n)     {         printf("%d",n/a);     }     else if(b>0 && a<n)     {         printf("%d",(n/a)+1);     }     else if(n<a)     {         printf("%d",1);     } }

---
+++ Bags_and_Candies.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,m;
+    scanf("%d%d%d",&n,&k,&m);
+    int a=k*m,b=n%a;
+    if(b==0 && a<=n)
+    {
+        printf("%d",n/a);
+    }
+    else if(b>0 && a<n)
+    {
+        printf("%d",(n/a)+1);
+    }
+    else if(n<a)
+    {
+        printf("%d",1);
+    }
+}
