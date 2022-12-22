9a832caace5ae5b33699e3337d2a06828122c0a9
RAGHUVEER1A04J5 <119720122+RAGHUVEER1A04J5@users.noreply.github.com>
2022-12-22 20:55:26 +0530

    #include<stdio.h> int sum(int n) {     int sum=0;     while(n!=0)     {         sum=sum+n%10;         n=n/10;     }     return sum; } int main() {     int n,d;     scanf("%d",&n);     while(n>9)     {         n=sum(n);     }     printf("%d",n); }

---
+++ Add_Digits.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int sum(int n)
+{
+    int sum=0;
+    while(n!=0)
+    {
+        sum=sum+n%10;
+        n=n/10;
+    }
+    return sum;
+}
+int main()
+{
+    int n,d;
+    scanf("%d",&n);
+    while(n>9)
+    {
+        n=sum(n);
+    }
+    printf("%d",n);
+}
