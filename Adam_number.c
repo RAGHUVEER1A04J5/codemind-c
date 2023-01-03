6705db56a7fd37798e56a7876d67a72867bf7deb
RAGHUVEER1A04J5 <119720122+RAGHUVEER1A04J5@users.noreply.github.com>
2023-01-03 21:17:01 +0530

    #include<stdio.h> int rev(int n) {     int rev=0;     while(n!=0)     {         rev=rev*10+n%10;         n=n/10;     }     return rev; } int main() {     int m,n,a,b;     scanf("%d",&n);     a=n*n;     m=rev(n);     b=m*m;     if(rev(b)==a)     printf("True");     else     printf("False"); }

---
+++ Adam_number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int rev(int n)
+{
+    int rev=0;
+    while(n!=0)
+    {
+        rev=rev*10+n%10;
+        n=n/10;
+    }
+    return rev;
+}
+int main()
+{
+    int m,n,a,b;
+    scanf("%d",&n);
+    a=n*n;
+    m=rev(n);
+    b=m*m;
+    if(rev(b)==a)
+    printf("True");
+    else
+    printf("False");
+}
