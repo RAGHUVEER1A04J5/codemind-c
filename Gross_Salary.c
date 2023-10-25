48f732d23cf7df04f25eb00a7d311ac919a622fc
RAGHUVEER1A04J5 <119720122+RAGHUVEER1A04J5@users.noreply.github.com>
2023-10-25 19:48:39 +0530

    #include<stdio.h> int main() {     int bs;     float da,hra,gs;     scanf("%d",&bs);     if(bs<=10000)     {     da=(bs*80)/100;     hra=(bs*20)/100;     }     else if(bs<=20000)     {     da=(bs*90)/100;     hra=(bs*25)/100;     }     else     {     da=(bs*95)/100;     hra=(bs*30)/100;     }     gs=bs+da+hra;     {         printf("%.2f",gs);     } }

--- Gross_Salary.c
+++ Gross_Salary.c
@@ -1,24 +1,27 @@
+
 #include<stdio.h>
 int main()
 {
     int bs;
+    float da,hra,gs;
     scanf("%d",&bs);
-    float gs,da,hra;
     if(bs<=10000)
-  {
-      da=bs*80/100;
-      hra=bs*20/100;
-  }
-  else if(bs<=20000)
-  {
-      da=bs*90/100;
-      hra=bs*25/100;
-  }
-  else if(bs>20000)
-  {
-      da=bs*95/100;
-      hra=bs*30/100;
-  }
-  gs=bs+da+hra;
-  printf("%.2f",gs);
+    {
+    da=(bs*80)/100;
+    hra=(bs*20)/100;
+    }
+    else if(bs<=20000)
+    {
+    da=(bs*90)/100;
+    hra=(bs*25)/100;
+    }
+    else
+    {
+    da=(bs*95)/100;
+    hra=(bs*30)/100;
+    }
+    gs=bs+da+hra;
+    {
+        printf("%.2f",gs);
+    }
 }
