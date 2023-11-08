f65bec9b7154c3fadd9cbdd1574723b61bf5f2e2
RAGHUVEER1A04J5 <119720122+RAGHUVEER1A04J5@users.noreply.github.com>
2023-11-08 17:52:13 +0530

    #include<stdio.h> int main() {     int i,n,a,b=12,mul=1;     scanf("%d",&n);     for(i=1;i<=b;i++)     {         mul=n*i;         printf("%d x %d = %d ",n,i,n*i);     } }

---
+++ Multiplication_Table_-_1.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+    int i,n,a,b=12,mul=1;
+    scanf("%d",&n);
+    for(i=1;i<=b;i++)
+    {
+        mul=n*i;
+        printf("%d x %d = %d
+",n,i,n*i);
+    }
+}
+
+
