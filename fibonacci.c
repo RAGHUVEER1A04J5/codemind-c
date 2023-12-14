340cd61e8454212e57bbc4b784e914c43e54bd59
RAGHUVEER1A04J5 <119720122+RAGHUVEER1A04J5@users.noreply.github.com>
2023-12-14 23:27:55 +0530

    #include<stdio.h> int main() {     int i,n,t1=0,t2=1,nt=0;     scanf("%d",&n);     printf("%d %d ",t1,t2);     for(i=2;i<n;i++)     {         nt=t1+t2;         printf("%d ",nt);         t1=t2;         t2=nt;     } }

--- fibonacci.c
+++ fibonacci.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,a=0,b=1,i,c;
+    int i,n,t1=0,t2=1,nt=0;
     scanf("%d",&n);
-    printf("%d %d ",a,b);
+    printf("%d %d ",t1,t2);
     for(i=2;i<n;i++)
     {
-      c=a+b;
-      printf("%d ",c);
-      a=b;
-      b=c;
+        nt=t1+t2;
+        printf("%d ",nt);
+        t1=t2;
+        t2=nt;
     }
 }
